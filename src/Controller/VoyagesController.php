bfc4c2e0fd7e3870b08203051d1964a59c51d438
ELKA10 <97535179+ELKA10@users.noreply.github.com>
2024-03-19 16:23:21 +0100

    "Phase 8 : ORM (affichage en colonne des informations provenant de la BDD).

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -1,23 +1,41 @@
 <?php
 namespace App\Controller;
 
+use App\Repository\VisiteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * Description of VoyagesController
+ * Description of VoyagesConrtoller
  *
  * @author elkam
  */
 class VoyagesController extends AbstractController{
 
     /**
+     * 
+     * @var VisiteRepository
+     */
+    private $repository;
+
+    /**
+     * 
+     * @param VisiteRepository $repository
+     */
+    public function __construct(VisiteRepository $repository) {
+        $this->repository = $repository;
+    }
+
+    /**
      * @Route("/voyages", name="voyages")
      * @return Response
      */
-    public function index(): Response {
-        return $this->render("pages/voyages.html.twig");
+    public function index(): Response{
+        $visites = $this->repository->findAll();
+        return $this->render("pages/voyages.html.twig", [
+            'visites' => $visites
+        ]);
     }
 
 }
--- src/Entity/Visite.php
+++ src/Entity/Visite.php
@@ -92,6 +92,16 @@ class Visite
 
         return $this;
     }
+    
+    
+    public function getDatecreationString(): string
+    {
+        if($this->datecreation == null){
+            return "";
+        }else{
+            return $this->datecreation->format('d/m/Y');
+        }
+    }
 
     public function getNote(): ?int
     {
